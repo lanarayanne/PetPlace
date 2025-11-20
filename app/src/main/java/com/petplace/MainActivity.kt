c2a7b5b683c455cecb455616e648a840efd28c99
Lana <alana.rayanne@gmail.com>
2025-11-20 14:04:40 -0300

    add pages profile, pets, bookings and hostings

--- app/src/main/java/com/petplace/MainActivity.kt
+++ app/src/main/java/com/petplace/MainActivity.kt
@@ -1,27 +1,17 @@
 package com.petplace
 
-import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.petplace.ui.HomePage
 import com.petplace.ui.theme.PetPlaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -56,27 +46,3 @@ fun GreetingPreview() {
     }
 }
 
-@Composable
-fun HomePage(modifier: Modifier = Modifier) {
-    val activity = LocalActivity.current as Activity
-    Column(
-        modifier = modifier.padding(16.dp).fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = CenterHorizontally,
-    ) {
-        Text(
-            text = "Bem-vindo/a!",
-            fontSize = 24.sp
-        )
-
-        Row(modifier = modifier) {
-
-            Spacer(modifier = modifier.size(24.dp))
-            Button(
-                onClick = { activity.finish() },
-            ) {
-                Text("Sair")
-            }
-        }
-    }
-}
---
+++ app/src/main/java/com/petplace/ui/ProfilePage.kt
@@ -0,0 +1,33 @@
+package com.petplace.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ProfilePage(modifier: Modifier = Modifier.Companion) {
+    Column(
+        modifier = modifier.fillMaxSize()
+            .background(Color.White)
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Meu Perfil",
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF419D78),
+            modifier = modifier.align(CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
+    }
+}
